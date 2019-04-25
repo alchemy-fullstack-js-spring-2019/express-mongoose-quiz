6ca3f3f09938b5ee1426d8232c0a95a516207053
Leslie Shea <leslieyshea@gmail.com>
2019-04-25 13:43:15 -0700

    can get a list of colors test passing

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
+const Color = require('../lib/models/Color');
 
 describe('color routes', () => {
   beforeAll(() => {
@@ -37,6 +38,27 @@ describe('color routes', () => {
           _id: expect.any(String),
           __v: 0
         });
+      });
+  });
+
+  it('can get a list of colors', () => {
+    return Color.create({
+      name: 'fancy',
+      hex: '123FFF',
+      red: 5,
+      green: 100,
+      blue: 250
+    })
+      .then(() => {
+        return request(app)
+          .get('/api/v1/colors');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body).toEqual([{
+          _id: expect.any(String),
+          name: 'fancy'
+        }]);
       });
   });
 });
