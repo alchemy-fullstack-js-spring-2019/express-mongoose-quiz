2668668772e9710359847bd5ccdc530910394215
emily baier <emilyrbaier@gmail.com>
2019-04-25 13:45:53 -0700

    created first route, getting error in test

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
+const Color = require('../lib/models/Color');
 
 describe('color routes', () => {
   beforeAll(() => {
@@ -17,5 +18,35 @@ describe('color routes', () => {
     return mongoose.connection.close();
   });
 
-  it('add your tests', () => { });
+  it('can create a new color', () => {
+    return Color.create({
+      name: 'mint magic',
+      hex: '#54e5d4',
+      red: 84,
+      green: 229,
+      blue: 212
+    })
+      .then(color => {
+        return request(app)
+          .post('/colors')
+          .send({
+            name: color.name,
+            hex: color.hex,
+            red: color.red,
+            green: color.green,
+            blue: color.blue
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'mint magic',
+          hex: '#54e5d4',
+          red: 84,
+          green: 229,
+          blue: 212,
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
 });
