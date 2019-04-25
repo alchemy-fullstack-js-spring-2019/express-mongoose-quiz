29fb240176d417a74df875e8b18be52ea772b394
Anna Ridenour <annamridenour@gmail.com>
2019-04-25 13:44:09 -0700

    made and tested get all colors route

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
-// const Color = require('../lib/models/Color');
+const Color = require('../lib/models/Color');
 
 describe('color routes', () => {
   beforeAll(() => {
@@ -38,6 +38,27 @@ describe('color routes', () => {
           _id: expect.any(String),
           __v: 0
         });
+      });
+  });
+
+  it('can get a list of all colors', () => {
+    return Color.create({
+      name: 'Blue',
+      hex: '#4286f4',
+      red: 66,
+      green: 134, 
+      blue: 244 
+    })
+      .then(() => {
+        return request(app)
+          .get('/api/v1/colors');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body).toEqual([{
+          _id: expect.any(String),
+          name: 'Blue'
+        }]);
       });
   });
 });
