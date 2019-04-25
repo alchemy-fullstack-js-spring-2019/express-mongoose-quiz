ef62ffce3c3b0df1c15f63fbc269cd0672a06d56
Sean Nelson <smnelsonn@gmail.com>
2019-04-25 13:40:19 -0700

    get list working

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
+const Color = require('../lib/models/Color');
 
 describe('color routes', () => {
   beforeAll(() => {
@@ -37,6 +38,24 @@ describe('color routes', () => {
           __v: 0,
           _id: expect.any(String)
         });
+      });
+  });
+  it('gets a list of colors', () => {
+    return request(app)
+      .post('/api/v1/colors')
+      .send({
+        name: 'test',
+        hex: 'abc',
+        red: 3,
+        green: 5,
+        blue: 1
+      })
+      .then(() => {
+        return request(app)
+          .get('/api/v1/colors')
+          .then(resList => {
+            expect(resList.body).toHaveLength(1);
+          });
       });
   });
 });
