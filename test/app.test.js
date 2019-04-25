d31edb2de4a5ca3754242975b2215948a5b1f86c
emily baier <emilyrbaier@gmail.com>
2019-04-25 13:56:31 -0700

    get by id route written and passes test

--- test/app.test.js
+++ test/app.test.js
@@ -19,13 +19,14 @@ describe('color routes', () => {
   });
 
   it('can create a new color', () => {
-    return Color.create({
-      name: 'mint magic',
-      hex: '#54e5d4',
-      red: 84,
-      green: 229,
-      blue: 212
-    })
+    return Color
+      .create({
+        name: 'mint magic',
+        hex: '#54e5d4',
+        red: 84,
+        green: 229,
+        blue: 212
+      })
       .then(color => {
         return request(app)
           .post('/api/v1/colors')
@@ -50,20 +51,46 @@ describe('color routes', () => {
       });
   });
 
-  it('can get a list of colors', ()=> {
-    return Color.create({
-      name: 'mint magic',
-      hex: '#54e5d4',
-      red: 84,
-      green: 229,
-      blue: 212
-    })
+  it('can get a list of colors', () => {
+    return Color
+      .create({
+        name: 'mint magic',
+        hex: '#54e5d4',
+        red: 84,
+        green: 229,
+        blue: 212
+      })
       .then(()=> {
         return request(app)
           .get('/api/v1/colors');
       })
       .then(res => {
         expect(res.body).toHaveLength(1);
+      });
+  });
+
+  it('can get a color by id', () => {
+    return Color
+      .create({
+        name: 'mint magic',
+        hex: '#54e5d4',
+        red: 84,
+        green: 229,
+        blue: 212
+      })
+      .then(createdColor => {
+        return request(app)
+          .get(`/api/v1/colors/${createdColor._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'mint magic',
+          hex: '#54e5d4',
+          red: 84,
+          green: 229,
+          blue: 212,
+          _id: expect.any(String)
+        });
       });
   });
 });
