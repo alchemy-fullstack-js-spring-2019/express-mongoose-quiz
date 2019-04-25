9eaa887a3b43ce11b11f3a077811c93afb2347b8
Masmics <cherlocker3113@gmail.com>
2019-04-25 14:20:59 -0700

    committing, not done

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const request = require('supertest');
+const chance = require('chance')();
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
-const colors = require('../lib/routes/colors');
+//const colors = require('../lib/routes/colors');
 const Color = require('../lib/models/Color');
+
+function createColor() {
+  return Color
+    .create({
+      name: chance.name(),
+      hex: '#6666ff',
+      red: 35,
+      green: 20,
+      blue: 200
+    });
+}
 
 describe('color routes', () => {
   beforeAll(() => {
@@ -19,44 +31,26 @@ describe('color routes', () => {
     return mongoose.connection.close();
   });
 
-  it('POST route, creates new color and returns it', () => {
-    return Color.create({
-      name: 'ColorIt',
-      hex: '#6666ff',
-      red: 35,
-      green: 20,
-      blue: 200
-    })
-      .then(res => {
-        expect(res.body).toEqual({
-          name: 'ColorIt',
-          hex: '#6666ff',
-          red: 35,
-          green: 20,
-          blue: 200,
-          _id: expect.any(String),
-          __v: expect.any(Number)
-        });
-      });
-  });
-
-  // it('GET route respond with list of colors, w. only ID and name', () => {
-
-
+  // it('POST route, creates new color and returns it', () => {
+  //   return createColor()
+  //     .then(color => {
+  //       return request(app)
+  //         .post('/api/v1/colors')
+  //         .send(color);
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         name: expect.any(String),
+  //         hex: '#6666ff',
+  //         red: 35,
+  //         green: 20,
+  //         blue: 200,
+  //         _id: expect.any(String),
+  //         //__v: expect.any(Number)
+  //       });
+  //     });
   // });
+  it('exists', () => {
+    expect(0).toEqual(0);
+  });
 });
-
-// it('can get a list of tweets', () => {
-//   return User.create({ handle: 'chris', name: 'bo-biss', email: 'AndInDarkness@Bind.Them' })
-//     .then(user => {
-//       return Tweet.create({ user: user._id, body: 'my LOTR tweet' });
-//     })
-//     .then(() => {
-//       return request(app)
-//         .get('/tweets/all');
-//     })
-//     .then(res => {
-//       expect(res.body).toHaveLength(1);
-//     });
-// });
-
