b3963d4e119acc974243dfeddbf8d9610747d3a3
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-25 13:51:11 -0700

    all done

--- test/app.test.js
+++ test/app.test.js
@@ -17,5 +17,88 @@ describe('color routes', () => {
     return mongoose.connection.close();
   });
 
-  it('add your tests', () => { });
+  const testColor = {
+    name: 'blue',
+    hex: '	#0000FF',
+    red: 0,
+    green: 0,
+    blue: 255
+  };
+
+  it('creates a new color', async() => {
+    const res = await request(app)
+      .post('/api/v1/colors')
+      .send(testColor);
+
+    expect(res.body).toEqual({
+      _id: expect.any(String),
+      __v: 0,
+      name: 'blue',
+      hex: '	#0000FF',
+      red: 0,
+      green: 0,
+      blue: 255
+    });
+  });
+
+  it('gets a list of colors', async() => {
+    await request(app)
+      .post('/api/v1/colors')
+      .send(testColor);
+
+    const res = await request(app)
+      .get('/api/v1/colors');
+
+    expect(res.body).toHaveLength(1);
+    expect(res.body[0]).toEqual({
+      _id: expect.any(String),
+      name: 'blue'
+    });
+  });
+
+  it('gets a color by id', async() => {
+    const createdRes = await request(app)
+      .post('/api/v1/colors')
+      .send(testColor);
+
+    const res = await request(app)
+      .get(`/api/v1/colors/${createdRes.body._id}`);
+    
+    expect(res.body).toEqual({
+      _id: createdRes.body._id.toString(),
+      name: 'blue',
+      hex: '	#0000FF',
+      red: 0,
+      green: 0,
+      blue: 255
+    });
+  });
+
+  it('can update a color name by id', async() => {
+    const createdRes = await request(app)
+      .post('/api/v1/colors')
+      .send(testColor);
+
+    const res = await request(app)
+      .patch(`/api/v1/colors/${createdRes.body._id}`)
+      .send({ name: 'really blue', hex: 'ignore this' });
+
+    expect(res.body).toEqual({
+      _id: createdRes.body._id.toString(),
+      name: 'really blue'
+    });
+  });
+
+  it('can delete a color by id', async() => {
+    const createdRes = await request(app)
+      .post('/api/v1/colors')
+      .send(testColor);
+
+    const res = await request(app)
+      .delete(`/api/v1/colors/${createdRes.body._id}`);
+
+    expect(res.body).toEqual({
+      _id: createdRes.body._id.toString()
+    });
+  });
 });
