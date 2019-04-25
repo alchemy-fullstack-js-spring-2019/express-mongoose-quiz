c881e0de9231368f370541bc9e598a7fd6b1a592
dreamer <wildetangeant@gmail.com>
2019-04-25 14:09:43 -0700

    wroking on post

--- test/app.test.js
+++ test/app.test.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
+// const colorSchema = require('../lib/models/Color');
+
 
 describe('color routes', () => {
   beforeAll(() => {
     return connect();
   });
-
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
@@ -17,5 +18,24 @@ describe('color routes', () => {
     return mongoose.connection.close();
   });
 
-  it('add your tests', () => { });
+  it('can can create a new color', () => {
+    return request(app)
+      .post('/api/v1/colors')
+      .send({
+        name: 'color1',
+        hex: '444444',
+        red: '22',
+        blue: '22',
+        green: '22'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'color1',
+          hex: '444444',
+          red: '22',
+          blue: '22',
+          green: '22'
+        });
+      });
+  });
 });
