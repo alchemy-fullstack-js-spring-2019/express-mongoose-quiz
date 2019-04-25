837549c12edea918a1834d6621a01846a612626f
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-25 13:50:46 -0700

    find color by ID test passing

--- test/app.test.js
+++ test/app.test.js
@@ -57,5 +57,29 @@ describe('color routes', () => {
         expect(res.body).toHaveLength(1);
       });
   });
+  it('gets a color by id', () => {
+    return ColorSchema
+      .create({
+        name: 'majenta',
+        hex: '88943',
+        red: 200,
+        green: 20,
+        blue: 60
+      })
+      .then(createdColor => {
+        return request(app)
+          .get(`/api/v1/colors/${createdColor._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'majenta',
+          hex: '88943',
+          red: 200,
+          green: 20,
+          blue: 60,
+          _id: expect.any(String),
+        });
+      });
+  });
 });
 
