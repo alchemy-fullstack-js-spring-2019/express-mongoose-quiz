62c3bbc57225be96e7d095f90f38ca294d4c6f13
ollinieminen <otn987@gmail.com>
2019-04-25 13:48:12 -0700

    all code but cant test :(

--- test/app.test.js
+++ test/app.test.js
@@ -17,5 +17,84 @@ describe('color routes', () => {
     return mongoose.connection.close();
   });
 
-  it('add your tests', () => { });
+  it('can create color', () => { 
+    return request(app)
+      .post('/api/v1/colors')
+      .send({
+        name:'test post',
+        hex:'my favorite hex',
+        red:10,
+        green:20,
+        blue:30
+      })
+      .then(newColor=>{
+        expect(newColor.body).toEqual({
+          name:'test post',
+          hex:'my favorite hex',
+          red:10,
+          green:20,
+          blue:30
+        });
+      });
+
+  });
+  it('can get a list of colors', ()=>{
+    return request(app)
+      .post('/api/v1/colors')
+      .send({
+        name:'test post',
+        hex:'my favorite hex',
+        red:10,
+        green:20,
+        blue:30
+      })
+      .then(()=>{
+        return request(app)
+          .get('api/v1/colors');
+      })
+      .then(found=>{
+        expect(found).toHaveLength(1);
+      });
+   
+  });
+  it('can get a color by id', ()=>{
+    return request(app)
+      .post('/api/v1/colors')
+      .send({
+        name:'test post',
+        hex:'my favorite hex',
+        red:10,
+        green:20,
+        blue:30
+      })
+      .then(createdColor=>{
+        const colorId = createdColor.body.colorId;
+        return request(app)
+          .get(`api/v1/colors/${colorId}`);
+      })
+      .then(found=>{
+        expect(found).toHaveLength(1);
+      });
+
+  });
+  it('can delete a color by id', ()=>{
+    return request(app)
+      .post('/api/v1/colors')
+      .send({
+        name:'test post',
+        hex:'my favorite hex',
+        red:10,
+        green:20,
+        blue:30
+      })
+      .then(createdColor=>{
+        const colorId = createdColor.body.colorId;
+        return request(app)
+          .delete(`api/v1/colors/${colorId}`);
+      })
+      .then(found=>{
+        expect(found).toHaveLength(0);
+      });
+
+  });
 });
