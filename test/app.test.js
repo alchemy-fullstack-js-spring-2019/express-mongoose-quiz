4621b70d545d1d1e60ac6b8d5260b10431b87bbb
Peter Kotowski <bighoopee@gmail.com>
2019-04-25 14:26:20 -0700

    end of quiz

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
+const Color = require('../lib/models/Color');
 
 describe('color routes', () => {
   beforeAll(() => {
@@ -38,6 +39,19 @@ describe('color routes', () => {
           __v: 0
         
         });
+      });
+  });
+  it('can get a list of colors', () => {
+    return Color
+      .create({ name: 'pink', hexValue:'', red: 111, blue: 23, green: 149 })
+      .then(() => {
+        return request(app)
+          .get('/api/v1/colors');
+
+
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
       });
   });
 });
